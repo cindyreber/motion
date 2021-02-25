5922fd3e099412ffe4fcf55a33c22b84dc1aba86
ezequias calvo ladmann <calvo.ezequias@gmail.com>
2021-02-25 08:38:11 -0300

    getPostsList and rendered

--- src/components/navbar/index.jsx
+++ src/components/navbar/index.jsx
@@ -8,11 +8,12 @@ import postsLogo from "../LeftLogin/assets/images/posts_logo.png";
 import IconFriends from "../../layout/svgs/IconFriends";
 import Bell from "../../layout/svgs/Bell";
 import Menu from "../../layout/svgs/Menu";
+import history from "../../history";
 // import search from "../LeftLogin/assets/svgs/search_icon.svg"
 import jennifer from "../LeftLogin/assets/images/users/jennifer.png";
 
 const Wrapper = styled.div`
-width: 100vw;
+  width: 100vw;
 `;
 
 const NavContainer = styled.nav`
@@ -29,43 +30,43 @@ const LeftItems = styled.section`
 `;
 
 const MotionLogoContainer = styled.div`
-display: flex;
-align-items: center;
-padding: 0 80px 0 20px;
+  display: flex;
+  align-items: center;
+  padding: 0 80px 0 20px;
 `;
 
 const MotionLogo = styled.img`
-width: 30px;
-height: 30px;
-padding: 30px 10px;
+  width: 30px;
+  height: 30px;
+  padding: 30px 10px;
 `;
 
 const PostsLogoContainer = styled.div`
-display: flex;
-align-items: center;
-margin-right: 50px;
+  display: flex;
+  align-items: center;
+  margin-right: 50px;
 
-:hover {
+  :hover {
     border-bottom: 2px solid #b192fe;
-}
+  }
 `;
 
 const PostsLogo = styled.img`
-width: 20px;
-height: 20px;
-padding: 10px;
+  width: 20px;
+  height: 20px;
+  padding: 10px;
 `;
 
 const FriendsLogoContainer = styled.div`
-display: flex;
-align-items: center;
-& p {
+  display: flex;
+  align-items: center;
+  & p {
     padding: 10px;
-}
+  }
 
-:hover {
+  :hover {
     border-bottom: 2px solid #b192fe;
-}
+  }
 `;
 
 const RightItems = styled.section`
@@ -78,107 +79,48 @@ const RightItems = styled.section`
 `;
 
 const Avatar = styled.img`
-padding: 0 30px;
+  padding: 0 30px;
 `;
-
-// const SearchContainer = styled.div`
-// margin: 0 auto;
-//  width: 100%;
-//  max-width: 1200px; 
-//  height: 80px;
-//  display: flex;
-//  justify-content: space-between;
-//  align-items: center;
-//  border-bottom: 1px solid #f5f5f5;
-// `;
-
-// const SearchBar = styled.div`
-//  display: flex;
-//  align-items: center;
-
-// `;
-
-// const InputField = styled.input`
-// border: none;
-// padding: 20px;
-// background: transparent;
-// margin-left: 20px;
-
-// :focus {
-//     outline: none !important;
-//     box-shadow: 0 0 10px #b192fe;
-//     //border:1px solid #b192fe;
-// }`;
-
-// const Magnifyer = styled.img`
-// padding: 2px;
-// `;
-
-// const RightButtonContainer = styled.div`
-//  display: flex;
-// `;
-
-// const RightButton = styled.p`
-//     margin: 0 20px;
-//     color: grey;
-//     padding: 30px 0;
-
-//     :hover {
-//         border-bottom: 2px solid black;
-//         color: black;
-//     }
-// `;
 
 const NavBar = () => {
   const user = useSelector((state) => state.userData);
   const dispatch = useDispatch();
-  console.log(user, "in user");
 
   useEffect(async () => {
-    const response = await ApiProfile();
-    console.log(response, "from useEffect");
-    dispatch(ProfileData(response));
+    if (localStorage.getItem("token")) {
+      const response = await ApiProfile();
+      dispatch(ProfileData(response));
+    } else {
+      history.push("/");
+    }
   }, []);
 
   return (
-      <Wrapper>
-        <NavContainer>
-            <LeftItems>
-            <MotionLogoContainer>
-                <MotionLogo src={motionLogo} alt="motionlogo" />
-                <h3>Motion</h3>
-            </MotionLogoContainer>
+    <Wrapper>
+      <NavContainer>
+        <LeftItems>
+          <MotionLogoContainer>
+            <MotionLogo src={motionLogo} alt="motionlogo" />
+            <h3>Motion</h3>
+          </MotionLogoContainer>
 
-            <PostsLogoContainer>
-                <PostsLogo src={postsLogo} alt="postsLogo" />
-                <p>Posts</p>
-            </PostsLogoContainer>
+          <PostsLogoContainer>
+            <PostsLogo src={postsLogo} alt="postsLogo" />
+            <p>Posts</p>
+          </PostsLogoContainer>
 
-            <FriendsLogoContainer>
-                <IconFriends />
-                <p>Find Friends</p>
-            </FriendsLogoContainer>
-            </LeftItems>
+          <FriendsLogoContainer>
+            <IconFriends />
+            <p>Find Friends</p>
+          </FriendsLogoContainer>
+        </LeftItems>
 
-            <RightItems>
-                <Bell />
-                <Avatar src={jennifer} alt="jennifer" />
-                <Menu />
-            </RightItems>
-        </NavContainer>
-
-        {/* <SearchContainer>
-            <SearchBar>
-                <Magnifyer src={search}/>
-                <InputField type="text" placeholder="Search posts..."></InputField>
-            </SearchBar>
-
-            <RightButtonContainer>
-                <RightButton>Liked</RightButton>
-                <RightButton>Friends</RightButton>
-                <RightButton>Follow</RightButton>
-            </RightButtonContainer>
-        </SearchContainer> */}
+        <RightItems>
+          <Bell />
+          <Avatar src={jennifer} alt="jennifer" />
+          <Menu />
+        </RightItems>
+      </NavContainer>
     </Wrapper>
   );
 };
--- src/layout/svgs/Bell.js
+++ src/layout/svgs/Bell.js
@@ -13,9 +13,9 @@ const Bell = () => {
       <g
         id="App"
         stroke="none"
-        stroke-width="1"
+        strokeWidth="1"
         fill="none"
-        fill-rule="evenodd"
+        fillRule="evenodd"
         opacity="0.300000012"
       >
         <g
