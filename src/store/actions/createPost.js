850c9bc9005035d20e63a586b545ed9a6b6293bf
ezequias calvo ladmann <calvo.ezequias@gmail.com>
2021-02-25 15:56:49 -0300

    small change

--- src/store/actions/createPost.js
+++ src/store/actions/createPost.js
@@ -1,11 +1,9 @@
 import { createPostApi } from "../../api/apiPosts";
-import history from "../../history";
 
 const createPost = (newPost) => async (dispatch) => {
   console.log("something");
   const response = await createPostApi(newPost, "posts/");
   dispatch({ type: "CREATE_POST", payload: response });
-  history.push("/feed");
 };
 
 export default createPost;
