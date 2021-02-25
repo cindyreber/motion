4930dbeb52a81495df52e9c1bd4a5f31b9533edf
ezequias calvo ladmann <calvo.ezequias@gmail.com>
2021-02-25 05:37:10 -0300

    redirect to feed page

--- src/store/actions/login.js
+++ src/store/actions/login.js
@@ -5,6 +5,7 @@ const logIn = (credentials, history) => async (dispatch) => {
   const response = await loginPost(credentials);
   dispatch({ type: "SEND_LOGINDATA", payload: response.data.access });
   localStorage.setItem("token", response.data.access);
+  history.push("/feed");
 };
 
 export default logIn;
